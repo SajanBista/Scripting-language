a7cf1d16acba82dbd158c6f393754a4e43cf524f
SajanBista <162802857+SajanBista@users.noreply.github.com>
2025-01-06 17:18:11 +0545

    these were some practices and will come more

---
+++ external.js
@@ -0,0 +1,15 @@
+/*document.write("i am the use of external js file");
+
+document.getElementById("welcome").innerHTML="this is the example of getting element by id";*/
+function myFunction(){
+    alert('this is the example of alter function');
+}
+function consoleFunction(){
+    console.log("this is the section for console log example");
+}
+function variable(){
+        var name ="ram";
+        var age ="22";
+        console.log('i am',name,'and','i am ',age,'years old');
+        
+}
